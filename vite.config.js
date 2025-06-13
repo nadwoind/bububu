11edd67e4332e741d6a83185a0909022e74b54c1
Maciej Filek <maciejfilek62@gmail.com>
2025-06-13 20:54:49 +0200

    csdadwsa

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import { resolve } from 'path';
 
 export default defineConfig({
   base: '/bububu/',
- root: '.',
+  root: '.',
   build: {
-
     outDir: 'docs',
-
     rollupOptions: {
-      
       input: {
         index: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'login.html'),
